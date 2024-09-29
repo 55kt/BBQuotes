09df1be0be6677cbf80960f2ff8234a7728538f7
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-29 20:01:32 +0300

    first commit & assets

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Vlad on 29/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
